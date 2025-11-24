3bd5669a1146753471ce7b3c9b6e5e6bdb096407
frittas <faelfreitas00@gmail.com>
2025-11-24 19:42:52 -0300

    rollback no comentario e adicionado pycache ao gitignore

--- wikipedia_edges.py
+++ wikipedia_edges.py
@@ -27,34 +27,34 @@ def gerar_edges(start_title, end_title, max_depth, max_paths):
     [('Neymar', 'Estados Unidos'), ('Estados Unidos', 'Barack Obama')]
     """
 
-    # queue = deque([(start_title, [start_title])])
-    # visited = set()
-    # found_paths = []
+    queue = deque([(start_title, [start_title])])
+    visited = set()
+    found_paths = []
 
-    # while queue and len(found_paths) < max_paths:
-    #     current_page, path = queue.popleft()
+    while queue and len(found_paths) < max_paths:
+        current_page, path = queue.popleft()
 
-    #     if current_page in visited:
-    #         continue
-    #     visited.add(current_page)
+        if current_page in visited:
+            continue
+        visited.add(current_page)
 
-    #     if len(path) > max_depth:
-    #         continue
+        if len(path) > max_depth:
+            continue
 
-    #     links = get_links(current_page)
+        links = get_links(current_page)
 
-    #     for link in links:
-    #         new_path = path + [link]
+        for link in links:
+            new_path = path + [link]
 
-    #         if link == end_title:
-    #             found_paths.append(new_path)
-    #             if len(found_paths) >= max_paths:
-    #                 break
-    #         else:
-    #             queue.append((link, new_path))
+            if link == end_title:
+                found_paths.append(new_path)
+                if len(found_paths) >= max_paths:
+                    break
+            else:
+                queue.append((link, new_path))
 
     # ---- extrai somente arestas dos caminhos encontrados ----
-    edges = [('Neymar', 'Estados Unidos'), ('Estados Unidos', 'Barack Obama'), ('Neymar', 'Joe Biden'), ('Joe Biden', 'Barack Obama')] # PARA TESTES USAR SOMENTE ESSA LINHA E COMENTAR AS OUTRAS DESSA FUNCAO
+    # edges = [('Neymar', 'Estados Unidos'), ('Estados Unidos', 'Barack Obama'), ('Neymar', 'Joe Biden'), ('Joe Biden', 'Barack Obama')] # PARA TESTES USAR SOMENTE ESSA LINHA E COMENTAR AS OUTRAS DESSA FUNCAO
     # edges = [
     #     ('A', 'B'),('B', 'C'),
     #     ('A', 'D'),('D', 'C'),
@@ -63,17 +63,17 @@ def gerar_edges(start_title, end_title, max_depth, max_paths):
     #     ('A', 'G'),('G', 'C'),
     # ]
 
-    # edges = []
+    edges = []
 
-    # seen = set()
+    seen = set()
 
-    # for path in found_paths:
-    #     for i in range(len(path) - 1):
-    #         edge = (path[i], path[i + 1])
-    #         if edge not in seen:
-    #             edges.append(edge)
-    #             seen.add(edge)
-    # print(edges)
+    for path in found_paths:
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            if edge not in seen:
+                edges.append(edge)
+                seen.add(edge)
+    print(edges)
     return edges
 
 
